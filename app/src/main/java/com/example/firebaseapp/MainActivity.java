bcdafdb94e4b200c7793856ec72249d8f82f98e4
Kolchin13D <kolchin13d@gmail.com>
2024-07-11 16:39:27 +0300

    testing

--- app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -4,22 +4,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button button;
     EditText editText;
+    TextView textView;
     private DatabaseReference mDatabase;
 
     @Override
@@ -30,15 +37,33 @@ public class MainActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference("GoldPrice").child("price");
 
-        mDatabase.setValue("2000");
+        String price = mDatabase.getKey();
 
         button = findViewById(R.id.button);
         editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.price);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                textView.setText("Price of gold right now is " +
+                        snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(),
+                        "No changes", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDatabase.setValue(editText.getText().toString());
+                Toast.makeText(getApplicationContext(),
+                        "Price is changed ", Toast.LENGTH_SHORT).show();
             }
         });
 
